78b3bcb46e61e2879c3c031f4956aed61d03484b
Dennis Schmid <124032805+deasch@users.noreply.github.com>
2023-06-21 11:53:40 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,3 @@
-variable "aws_region" {
-  description = "The AWS region to create things in."
-  default     = "eu-central-1"
-}
-
-# ubuntu-trusty-14.04 (x64)
-variable "aws_amis" {
-  default = {
-    "us-east-1" = "ami-5f709f34"
-    "us-west-2" = "ami-7f675e4f"
-  }
-}
-
-variable "key_name" {
-  description = "Name of the SSH keypair to use in AWS."
-}
-
-
-
-
-
 # Specify the provider and access details
 provider "aws" {
   region = "eu-central-1"
